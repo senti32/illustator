f62d29206cf7f70f8c932fc69647ded2b753cbcb
senti32 <senti32@users.noreply.github.com>
2015-09-29 11:55:02 +0200

    Update t-testNoGauss.R

--- t-testNoGauss.R
+++ t-testNoGauss.R
@@ -6,15 +6,15 @@ xN=10000; yN=10000
 #y=matrix(rnorm(yN*sampleSize, 0, 1), ncol=sampleSize, nrow=yN)
 #x=matrix(rt(xN*sampleSize, 1, 0), ncol=sampleSize, nrow=xN)
 #y=matrix(rt(yN*sampleSize, 1, 0), ncol=sampleSize, nrow=yN)
-#x=matrix(rcauchy(xN*sampleSize, 0, 1), ncol=sampleSize, nrow=xN)
-#y=matrix(rcauchy(yN*sampleSize, 0, 1), ncol=sampleSize, nrow=yN)
+x=matrix(rcauchy(xN*sampleSize, 0, 1), ncol=sampleSize, nrow=xN)
+y=matrix(rcauchy(yN*sampleSize, 0, 1), ncol=sampleSize, nrow=yN)
 
 #x=matrix(rcauchy(xN*sampleSize, 1, 0), ncol=sampleSize, nrow=xN)
 #x=matrix(round(runif(xN*sampleSize,-1,1))*rchisq(xN*sampleSize, 1, 0)^10, ncol=sampleSize, nrow=xN)
 #y=matrix(rt(xN*sampleSize, 1, 0), ncol=sampleSize, nrow=xN)
 
-x=matrix(rsn(xN*sampleSize, 0, 5, 10), ncol=sampleSize, nrow=xN)
-y=matrix(rsn(xN*sampleSize, 0, 5, 10), ncol=sampleSize, nrow=yN)
+#x=matrix(rsn(xN*sampleSize, 0, 5, 10), ncol=sampleSize, nrow=xN)
+#y=matrix(rsn(xN*sampleSize, 0, 5, 10), ncol=sampleSize, nrow=yN)
 
 #x=as.real(rsn(xN*sampleSize, 0, 1, 5))
 #y=as.real(rsn(xN*sampleSize, 0, 1, 5))
